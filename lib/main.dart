c81a9a10b8de9ceee4c8e1bdddc644c479a135c7
THE HOUND XX <godsonezekiel31@gmail.com>
2021-09-02 14:03:43 +0300

    ''

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import '../pages/home.dart';
+final dummy=0;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
