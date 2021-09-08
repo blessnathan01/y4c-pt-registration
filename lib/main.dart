13a3b6521156b92eda1aee1aa43429f276f9857d
nengha <68301183+Nengha-John@users.noreply.github.com>
2021-09-08 15:17:04 +0300

    User profile page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:overlay_support/overlay_support.dart';
+import 'package:users_page/pages/userProfile.dart';
+import 'package:users_page/widgets/login.dart';
 import '../pages/home.dart';
-final dummy=0;
+
+final dummy = 0;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,7 +21,7 @@ class MyApp extends StatelessWidget {
     return OverlaySupport.global(
       child: MaterialApp(
         theme: ThemeData(fontFamily: 'Raleway'),
-        home: Home(),
+        home: Login(),
         routes: {'/a': (context) => userHomePage()},
       ),
     );
